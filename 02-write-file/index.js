f63ac316adb71e33ab4904cc63f21330a67c9435
Anastasiia Nikonova <anastaney@gmail.com>
2024-12-24 13:58:40 +0100

    feat: implement write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
+
+(() => {
+  const greeting = '👋Please enter text. Type "exit" or press Ctrl+C to quit.';
+  const farewell = 'See you!🙌';
+
+  const fileName = 'text.txt';
+  const pathName = path.join(__dirname, fileName);
+  const writeStream = fs.createWriteStream(pathName, { flags: 'a' });
+  const rl = readline.createInterface({ input, output });
+
+  console.log(greeting);
+
+  rl.on('line', (line) => {
+    if (line === 'exit') {
+      rl.close();
+    } else {
+      writeStream.write(`${line}\n`);
+    }
+  });
+
+  rl.on('close', () => {
+    console.log(farewell);
+    writeStream.close();
+  });
+})();
