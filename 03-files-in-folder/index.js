8690c21877785322c614c1c9e1b3a32310eca221
Anastasiia Nikonova <anastaney@gmail.com>
2025-01-15 07:00:12 +0100

    refactor: refactor files-in-folder function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,29 +1,31 @@
-// const fs = require('node:fs');
 const path = require('node:path');
 const { readdir } = require('node:fs/promises');
 const { stat } = require('node:fs/promises');
 
 const folderName = 'secret-folder';
 
-(async () => {
-  try {
-    const pathName = path.join(__dirname, folderName);
-    const dirEntries = await readdir(pathName, { withFileTypes: true });
-    const files = dirEntries.filter((dirEntry) => dirEntry.isFile());
-    const data = await Promise.all(
-      files.map(async (file) => {
-        const filePath = path.join(pathName, file.name);
-        const ext = path.extname(file.name);
-        const name = path.basename(file.name, ext);
-        const slicedExt = ext.slice(1);
-        const fileStat = await stat(filePath);
-        const size = fileStat.size;
-        return `${name} - ${slicedExt} - ${size}b`;
-      }),
-    );
+async function getFiles(pathName) {
+  const dirEntries = await readdir(pathName, { withFileTypes: true });
+  return dirEntries.filter((dirEntry) => dirEntry.isFile());
+}
 
-    console.log(data.join('\n'));
-  } catch (err) {
-    console.error(err);
-  }
-})();
+async function createFileData(file, pathName) {
+  const filePath = path.join(pathName, file.name);
+  const ext = path.extname(file.name);
+  const name = path.basename(file.name, ext);
+  const slicedExt = ext.slice(1);
+  const fileStat = await stat(filePath);
+  const size = fileStat.size;
+  return `${name} - ${slicedExt} - ${size}b`;
+}
+
+async function readFilesData() {
+  const pathName = path.join(__dirname, folderName);
+  const files = await getFiles(pathName);
+  const data = await Promise.all(
+    files.map(async (file) => await createFileData(file, pathName)),
+  );
+  console.log(data.join('\n'));
+}
+
+readFilesData();
