a0cf7e03088e1bfa8e2a537b312356cea4e950a3
Anastasiia Nikonova <anastaney@gmail.com>
2025-01-19 19:50:13 +0100

    refactor: refactor merge styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,43 +1,28 @@
 const path = require('node:path');
 const { readdir, writeFile, readFile } = require('node:fs/promises');
 
-async function getDirEntries(dirName) {
-  const pathName = path.join(__dirname, dirName);
-  const dirEntries = await readdir(pathName, { withFileTypes: true });
-  console.log(typeof dirEntries);
-  return dirEntries;
-}
-
-const filterFiles = (entries) => entries.filter((entry) => entry.isFile());
-
-const filterExtension = (files, ext) =>
-  files.filter((file) => path.extname(file.name) === ext);
-
-async function readFileFunc(dir, file) {
-  const filePath = path.join(__dirname, dir, file.name);
-  const data = await readFile(filePath, 'utf8');
-  return data;
-}
-async function writeFileFunc(destDir, destName, data) {
-  const destSrcPath = path.join(__dirname, destDir, destName);
-  await writeFile(destSrcPath, data);
-}
-
 async function mergeFiles({
   ext = '.css',
   srcDir = 'styles',
   destName = 'bundle.css',
   destDir = 'project-dist',
 }) {
-  const entries = await getDirEntries(srcDir);
-  const files = filterFiles(entries);
-  const styles = filterExtension(files, ext);
-  const filesData = await Promise.all(
-    styles.map(async (file) => readFileFunc(srcDir, file)),
-  );
+  const srcPath = path.join(__dirname, srcDir);
   const separator = '\n';
+  const destPath = path.join(__dirname, destDir, destName);
+  const entries = await readdir(srcPath, { withFileTypes: true });
+  const styles = entries.filter(
+    (entry) => entry.isFile() && path.extname(entry.name) === ext,
+  );
+  const filesData = await Promise.all(
+    styles.map(async (file) => {
+      const filePath = path.join(srcPath, file.name);
+      const data = await readFile(filePath, 'utf8');
+      return data;
+    }),
+  );
   const mergedFiles = filesData.join(separator);
-  await writeFileFunc(destDir, destName, mergedFiles);
+  await writeFile(destPath, mergedFiles);
 }
 
 mergeFiles({});
